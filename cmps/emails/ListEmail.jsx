c7b1330968b0bf6950ada7f61759bda49eab9caa
AbirNadav <pakakaw41@gmail.com>
2020-04-27 19:43:21 +0300

    !3212

--- cmps/emails/ListEmail.jsx
+++ cmps/emails/ListEmail.jsx
@@ -45,25 +45,27 @@ export default class ListEmail extends React.Component {
         return (
             <section className="email-section fade-in">
                 <form className="flex align-center justify-center" onSubmit={this.onFilter}>
-                    <img className="email-search-img smallerAnim" src="assets/img/search.png" alt=""/>
+                    <img className="email-search-img smallerAnim" src="assets/img/search.png" alt="" />
                     <input className="email-search" type="text" name='search' placeholder="Search" value={search} onChange={this.handleChange} />
                 </form>
                 <div className="emails-list flex column fade-in">
                     {emails.map((email, idx) => {
                         return (
                             <div onClick={() => this.props.isFocus(email)} className={` flex space-between align-center fade-in ${email.isRead ? 'email-isRead' : 'email-card'}`} key={idx}>
-                                 <img onClick={(event) => this.toggleStarEmail(event, email)} className="email-card-star biggerAnim" title="Save As Starred" src={`assets/img/${this.isStarred(email)}.png`} alt="" srcSet="" />
+                                <img onClick={(event) => this.toggleStarEmail(event, email)} className="email-card-star biggerAnim" title="Save As Starred" src={`assets/img/${this.isStarred(email)}.png`} alt="" srcSet="" />
                                 <h2 title="Name" className={`email-card-name ${email.isRead ? 'read-email-lowopcatiy' : ''}`}>{email.name}</h2>
-                                {/* <p className={`email-card-to ${email.isRead ? 'read-email-lowopcatiy' : ''}`}>{email.toEmail}</p> */}
-                               
-                                <p title="Subject" className={`email-card-to ${email.isRead ? 'read-email-lowopcatiy' : ''}`} > {email.subject}</p>
-                                
-                                <p title="Message" className={`email-card-msg ${email.isRead ? 'read-email-lowopcatiy' : ''}`} >{email.body}</p>
+                                <p className={`email-card-to ${email.isRead ? 'read-email-lowopcatiy' : ''}`}>{email.toEmail}</p>
+                                <div className="email-card-subject-body flex column">
+
+                                    <p title="Subject" className={`email-card-subject ${email.isRead ? 'read-email-lowopcatiy' : ''}`} > {email.subject}</p>
+                                    <p title="Message" className={`email-card-msg ${email.isRead ? 'read-email-lowopcatiy' : ''}`} >{email.body}</p>
+
+                                </div>
                                 <p title="Current Date" className={`email-card-date ${email.isRead ? 'read-email-lowopcatiy' : ''}`} > {email.date}</p>
                                 <div className="email-list-btns">
-                                <img onClick={(event) => this.props.onReplay(event, email)} className="email-card-star biggerAnim" title="Replay Email" src={`assets/img/replay.png`} alt="" srcSet="" />
-                                <img onClick={(event) => this.props.onRemoveEmail(event, email.id)} className="email-card-star smallerAnim" title="Delete" src={`assets/img/delete.png`} alt="" srcSet="" />
-                                <img onClick={(event) => this.props.toggleIsRead(event, email)} className="email-card-star smallerAnim" title="Toggle Read/Unread" src={`assets/img/${email.isRead ? 'read' : 'unread'}.png`} alt="" srcSet="" />
+                                    <img onClick={(event) => this.props.onReplay(event, email)} className="email-card-star biggerAnim" title="Replay Email" src={`assets/img/replay.png`} alt="" srcSet="" />
+                                    <img onClick={(event) => this.props.onRemoveEmail(event, email.id)} className="email-card-star smallerAnim" title="Delete" src={`assets/img/delete.png`} alt="" srcSet="" />
+                                    <img onClick={(event) => this.props.toggleIsRead(event, email)} className="email-card-star smallerAnim" title="Toggle Read/Unread" src={`assets/img/${email.isRead ? 'read' : 'unread'}.png`} alt="" srcSet="" />
                                 </div>
                             </div>
                         )
