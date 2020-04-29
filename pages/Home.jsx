1e629f00ce582ca561da5e3ab29ca50afad6b1f7
AbirNadav <pakakaw41@gmail.com>
2020-04-29 18:53:34 +0300

    231

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -4,13 +4,13 @@ export default class Home extends React.Component {
         return (
             <section className="main-home-container">
                 <div className="home-content-title flex justify-center column">
-                    <h2 className="home-main-title">Appsus Application</h2>
-                    <h4 className="home-subtitle">Three In One</h4>
+                    <h2 className="home-main-title tracking-in-expand">Appsus Application</h2>
+                    <h4 className="home-subtitle tracking-in-expand">Three In One</h4>
                 </div>
 
                 <div className="apps-container flex even">
 
-                    <div className="single-app flex">
+                    <div className="single-app flex  puff-in-center">
                         <img src="assets/img/books.png" className="single-app-img" alt="" />
                         <div className="single-app-content flex column even">
                             <div className="single-app-title">Books</div>
@@ -20,7 +20,7 @@ export default class Home extends React.Component {
                             <a className="single-app-btn books-btn" href="index.html#/books">Take a look</a>
                         </div>
                     </div>
-                    <div className="single-app flex align-center">
+                    <div className="single-app flex align-center bounce-in-top">
                         <img src="assets/img/notes.png" className="single-app-img" alt="" />
                         <div className="single-app-content flex column even">
                             <div className="single-app-title">Notes</div>
@@ -30,7 +30,7 @@ export default class Home extends React.Component {
                             <a className="single-app-btn notes-btn" href="index.html#/notes">Take a look</a>
                         </div>
                     </div>
-                    <div className="single-app flex">
+                    <div className="single-app flex roll-in-bottom">
                         <img src="assets/img/books.png" className="single-app-img" alt="" />
                         <div className="single-app-content flex column even">
                             <div className="single-app-title">Email</div>
